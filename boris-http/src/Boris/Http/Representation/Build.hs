92263960086095316e51175ed09d4f9e598ebb40
mth <mark@hibberd.id.au>
2016-03-18 23:53:53 +1100

    Compilation.

--- boris-http/src/Boris/Http/Representation/Build.hs
+++ boris-http/src/Boris/Http/Representation/Build.hs
@@ -4,42 +4,18 @@ module Boris.Http.Representation.Build (
     GetBuild (..)
   ) where
 
-
-import           Airship (Resource (..), Webmachine, defaultResource, lookupParam, halt, putResponseBody, appendRequestPath)
-
-import           Boom.Data (Boom (..))
-import           Boom.Airship (boom, notfound)
-
 import           Boris.Core.Data
 import           Boris.Http.Data
-import           Boris.Http.Repository
-import           Boris.Http.Version
 import           Boris.Store.Build (BuildData (..))
-import qualified Boris.Store.Build as SB
-import qualified Boris.Store.Tick as ST
 import           Boris.Queue (BuildQueue (..), Request (..))
-import qualified Boris.Queue as Q
-
-import           Charlotte.Airship (PostHandler (..), withVersionJson)
-import           Charlotte.Airship (processPostMedia, jsonResponse, setResponseHeader)
-
-import           Control.Monad.IO.Class (liftIO)
 
 import           Data.Aeson (ToJSON (..), object, (.=))
 import           Data.Text (Text)
 
 import           Jebediah.Data (GroupName (..), StreamName (..))
 
-import           Mismi (runAWS, runAWST, renderError)
-import           Mismi.Amazonka (Env)
-
-import qualified Network.HTTP.Types as HTTP
-
 import           P
 
-import           System.IO (IO)
-
-import           X.Control.Monad.Trans.Either (EitherT, runEitherT, bimapEitherT)
 
 newtype GetBuild =
   GetBuild BuildData
