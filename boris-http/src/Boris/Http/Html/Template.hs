a1d7a16ea2b78db71d482324776bb2b08457bfd1
Tim Humphries <tim.humphries@ambiata.com>
2017-01-11 14:28:35 +1100

    Display 'build again' button always

--- boris-http/src/Boris/Http/Html/Template.hs
+++ boris-http/src/Boris/Http/Html/Template.hs
@@ -145,7 +145,6 @@ build l b =
       , ("ko", maybe BMXNull (BMXBool . (==) BuildKo) $ buildDataResult b)
       , ("undecided", maybe (BMXBool True) (const $ BMXNull) $ buildDataResult b)
       , ("log", maybe BMXNull (const $ BMXBool True) $ buildDataLog b)
-      , ("rebuild", case buildDataResult b of { Just (BuildKo) -> BMXBool True; _ -> BMXBool False })
       , ("cancel", BMXBool ((isNothing . buildDataResult $ b) && (notCancelled (buildDataCancelled b))))
       ]
   in
