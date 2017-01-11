a6f1a26a0278390f6be4a0f03339bd66c659095e
Stephen Underwood <_@sphvn.com>
2017-01-12 10:53:38 +1100

    Added a "Stale as of Date Time" message when unable to update

--- boris-http/assets/js/updateSelf.js
+++ boris-http/assets/js/updateSelf.js
@@ -1,4 +1,43 @@
-var fetchNew;
+var prettyDate, staleMsg, setStale, isStale, fetchNew;
+
+prettyDate = function(d) {
+  var months, hours, minutes, day, monthIndex, year;
+  
+  months = [
+      "Jan"
+    , "Feb"
+    , "Mar"
+    , "Apr"
+    , "May"
+    , "Jun"
+    , "Jul"
+    , "Aug"
+    , "Sept"
+    , "Oct"
+    , "Nov"
+    , "Dev"
+  ];
+
+  hours = d.getHours();
+  minutes = d.getMinutes();
+  day = d.getDate();
+  monthIndex = d.getMonth();
+  year = d.getFullYear();
+
+  return hours + ':' + minutes + ', ' + day + ' ' + months[monthIndex] + ' ' + year;
+}
+
+staleMsg = function(t) {
+  return "<h1 class=\"stale\"> Stale as of " + t + "</h1>";
+};
+
+setStale = function(elem) {
+  if (!isStale) {
+    isStale = true;
+    return elem.innerHTML = staleMsg(prettyDate(new Date()));
+  }
+}
+
 fetchNew = function(){
   var xhr;
   xhr = new XMLHttpRequest();
@@ -6,10 +45,20 @@ fetchNew = function(){
   xhr.responseType = "document";
   xhr.setRequestHeader('Accept', 'text/html');
   xhr.onload = function(){
-    if (xhr.status !== 200) return;
     var elem = document.getElementsByTagName('body')[0];
-    elem.parentNode.replaceChild(this.responseXML.body, elem);
+
+    if (xhr.status !== 200) {
+      return setStale(elem);
+    }
+
+    isStale = false;
+    return elem.parentNode.replaceChild(this.responseXML.body, elem);
+  };
+  xhr.onerror = function() {
+    var elem = document.getElementsByTagName('body')[0];
+    return setStale(elem);
   };
   return xhr.send();
 };
-window.setInterval(fetchNew, 1000 * 240);
+window.setInterval(fetchNew, 1000 * 120);
+
