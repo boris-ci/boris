2a25708ad2001fb8c2febadf4bae1c7d39dac38d
Charles O'Farrell <charleso@charleso.org>
2017-01-17 13:51:18 +1100

    Add asset middleware for readonly http

--- boris-http/main/boris-http-readonly.hs
+++ boris-http/main/boris-http-readonly.hs
@@ -6,6 +6,7 @@ import           Agriculture (agriculture)
 
 import           Boris.Core.Data
 import           Boris.Http.Config
+import qualified Boris.Http.Resource.Static as Static
 import           Boris.Http.Data
 import           Boris.Http.Route (borisReadonly)
 import qualified Boris.Store.Lifecycle as SL
@@ -34,5 +35,6 @@ main = do
   orDie renderError $ runAWS env $ SL.initialise e
 
   runStopFile (lookupEnv "BORIS_HTTP_STOP") $ \pin -> do
-    agriculture pin "boris-http-readonly" buildInfoVersion $ do
-      return $ resourceToWai defaultAirshipConfig (borisReadonly env e c) (resource404 ())
+    agriculture pin "boris-http-readonly" buildInfoVersion $
+      return . ($) Static.staticMiddleware $
+        resourceToWai defaultAirshipConfig (borisReadonly env e c) (resource404 ())
