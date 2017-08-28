65fdbeb41afe279d8ac84b4c546904a25697505c
mth <mark@hibberd.id.au>
2017-08-28 18:27:51 +1000

    Eventually true properties.

--- boris-store/test/Test/IO/Boris/Store/Results.hs
+++ boris-store/test/Test/IO/Boris/Store/Results.hs
@@ -8,6 +8,9 @@ import           Boris.Core.Data
 import           Boris.Store.Results (Result (..))
 import qualified Boris.Store.Results as Store
 
+import           Control.Monad.IO.Class (MonadIO (..))
+import qualified Control.Retry as Retry
+
 import           P
 
 import           Test.Boris.Core.Arbitrary ()
@@ -25,7 +28,8 @@ prop_add i p b r br =
 prop_add_fetch i p b r br =
   once . testAWS . withClean environment (Store.deleteItem environment) $ do
     _ <- runEitherT $ Store.add environment (Result i p b r br)
-    l <- runEitherT $ Store.fetch environment
+    l <- runEitherT . waitFor (not . null) $
+      Store.fetch environment
     pure $ l === Right [Result i p b r br]
 
 prop_add_compress_fetch p b r br =
@@ -33,14 +37,14 @@ prop_add_compress_fetch p b r br =
     _ <- runEitherT $ Store.add environment (Result (BuildId "10") p b master BuildKo)
     _ <- runEitherT $ Store.add environment (Result (BuildId "9") p b (Just r) br)
     z <- runEitherT $ Store.addWithCompressLimit environment 2 (Result (BuildId "8") p b (Just r) br)
-    l <- runEitherT $ Store.fetch environment
+    l <- runEitherT . waitFor (not . null) $ Store.fetch environment
     pure $ (z, l) === (Right (), Right [Result (BuildId "10") p b master BuildKo])
 
 prop_add_compress i p b r br =
   once . testAWS . withClean environment (Store.deleteItem environment) $ do
     _ <- runEitherT $ Store.add environment (Result i p b master BuildKo)
     _ <- runEitherT $ Store.add environment (Result i p b r br)
-    l <- runEitherT $ Store.compress environment
+    l <- runEitherT . waitFor (not . null) $ Store.compress environment
     pure $ l === Right [Result i p b master BuildKo]
 
 prop_add_compress_no_master i p b br =
@@ -53,6 +57,14 @@ prop_add_compress_no_master i p b br =
 master :: Maybe Ref
 master =
   Just $ Ref "refs/heads/master"
+
+waitFor :: MonadIO m => (b -> Bool) -> m b -> m b
+waitFor condition action =
+  Retry.retrying policy (const $ pure . condition) (const action)
+
+policy :: Retry.RetryPolicy
+policy =
+  Retry.constantDelay 200000 <> Retry.limitRetries 5
 
 return []
 tests = $quickCheckAll
