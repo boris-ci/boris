3b0fcc9f651bc52fd8639469897bca26be5156f8
Mark Hibberd <mark@hibberd.id.au>
2019-02-26 19:20:11 +1100

    Porting handlebars to projector.

---
+++ boris-http/gen/Boris/Http/Template/Page/Dashboard.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+module Boris.Http.Template.Page.Dashboard where
+import qualified Projector.Html.Runtime
+import Boris.Http.Template.Data.Build.Data
+import Boris.Http.Template.Data.Commit.Data
+pageDashboard :: Projector.Html.Runtime.Html
+pageDashboard = Projector.Html.Runtime.foldHtml [Projector.Html.Runtime.parentNode (Projector.Html.Runtime.Tag "p") (Projector.Html.Runtime.fold []) (Projector.Html.Runtime.foldHtml [Projector.Html.Runtime.textNodeUnescaped " It would be wonderful if this provided some useful information, but for now boris suggests the list of ",
+                                                                                                                                                                                       Projector.Html.Runtime.parentNode (Projector.Html.Runtime.Tag "a") (Projector.Html.Runtime.fold [[Projector.Html.Runtime.Attribute (Projector.Html.Runtime.AttributeKey "href") (Projector.Html.Runtime.AttributeValue "/project")]]) (Projector.Html.Runtime.textNodeUnescaped "projects"),
+                                                                                                                                                                                       Projector.Html.Runtime.textNodeUnescaped " may be more useful. "]),
+                                                 Projector.Html.Runtime.textNodeUnescaped " ",
+                                                 Projector.Html.Runtime.parentNode (Projector.Html.Runtime.Tag "p") (Projector.Html.Runtime.fold []) (Projector.Html.Runtime.foldHtml [Projector.Html.Runtime.textNodeUnescaped " ",
+                                                                                                                                                                                       Projector.Html.Runtime.parentNode (Projector.Html.Runtime.Tag "a") (Projector.Html.Runtime.fold [[Projector.Html.Runtime.Attribute (Projector.Html.Runtime.AttributeKey "href") (Projector.Html.Runtime.AttributeValue "/status")]]) (Projector.Html.Runtime.textNodeUnescaped "Build Statuses"),
+                                                                                                                                                                                       Projector.Html.Runtime.textNodeUnescaped " "]),
+                                                 Projector.Html.Runtime.textNodeUnescaped " ",
+                                                 Projector.Html.Runtime.parentNode (Projector.Html.Runtime.Tag "p") (Projector.Html.Runtime.fold []) (Projector.Html.Runtime.foldHtml [Projector.Html.Runtime.textNodeUnescaped " ",
+                                                                                                                                                                                       Projector.Html.Runtime.parentNode (Projector.Html.Runtime.Tag "a") (Projector.Html.Runtime.fold [[Projector.Html.Runtime.Attribute (Projector.Html.Runtime.AttributeKey "href") (Projector.Html.Runtime.AttributeValue "/scoreboard")]]) (Projector.Html.Runtime.textNodeUnescaped "Build Scoreboard"),
+                                                                                                                                                                                       Projector.Html.Runtime.textNodeUnescaped " "])]
