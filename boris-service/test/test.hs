25d559ebd5f25c14021abd7aba2fd18250efe6c7
Mark Hibberd <mark@hibberd.id.au>
2019-03-06 09:09:29 +1100

    Start on refresh of service.

--- boris-service/test/test.hs
+++ boris-service/test/test.hs
@@ -1,9 +1,22 @@
-import           Disorder.Core.Main
+{-# LANGUAGE NoImplicitPrelude #-}
+
+import           Control.Monad ((>>=), (>>), when, mapM)
+
+import           Prelude (($), (.), not, all, id)
+
+import qualified System.Exit as Exit
+import           System.IO (IO)
+import qualified System.IO as IO
 
 import qualified Test.Boris.Service.Git
+import qualified Test.IO.Boris.Service.Git
+import qualified Test.IO.Boris.Service.Workspace
+
 
 main :: IO ()
 main =
-  disorderMain [
+  IO.hSetBuffering IO.stdout IO.LineBuffering >> mapM id [
       Test.Boris.Service.Git.tests
-    ]
+    , Test.IO.Boris.Service.Git.tests
+    , Test.IO.Boris.Service.Workspace.tests
+    ] >>= \rs -> when (not . all id $ rs) Exit.exitFailure
