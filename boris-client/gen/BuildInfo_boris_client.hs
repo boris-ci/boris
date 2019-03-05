35144f32b93194affd4223f3dbf22cdee706e305
Mark Hibberd <mark@hibberd.id.au>
2019-03-06 08:59:31 +1100

    Update client to new build API.

--- boris-client/gen/BuildInfo_boris_client.hs
+++ boris-client/gen/BuildInfo_boris_client.hs
@@ -2,6 +2,6 @@ module BuildInfo_boris_client where
 import Prelude
 data RuntimeBuildInfo = RuntimeBuildInfo { buildVersion :: String, timestamp :: String, gitVersion :: String }
 buildInfo :: RuntimeBuildInfo
-buildInfo = RuntimeBuildInfo "0.0.1" "20190306085644" "462dc44-M"
+buildInfo = RuntimeBuildInfo "0.0.1" "20190306085757" "9070223-M"
 buildInfoVersion :: String
-buildInfoVersion = "0.0.1-20190306085644-462dc44-M"
+buildInfoVersion = "0.0.1-20190306085757-9070223-M"
